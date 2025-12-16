c26070467e3388e6423447c8d3a34d99072539f3
Harshit Deepak Kadam <143883021+kadamharshit@users.noreply.github.com>
2025-12-16 11:13:51 +0530

    Add files via upload

---
+++ num_application.py
@@ -0,0 +1,17 @@
+#basic arithmetic operation
+
+import numpy as np
+
+arr_1 = np.array([1,3,4,5,6])
+arr_2 = np.array([3,4,5,6,7])
+
+print(arr_1)
+
+'''
+#addition
+sum1 = arr_1 + arr_2
+#subtraciton
+sub1 = a - b
+
+print("Addition of two arrays:",sum1)
+'''
